f7eecffeb5d0dcf4ada9ef7b295254f262865c59
Miguel Mota <miguelmota2@gmail.com>
2017-10-17 09:46:20 -0700

    comment

--- contracts/InfiniteLoop.sol
+++ contracts/InfiniteLoop.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.4;
 
 contract MyContract {
   function loop() {
-    // i = 255 (uint8), causing an infinite loop.
+    // i will have max a max value 255 (initialized as uint8),
+    // causing an infinite loop.
     for (var i = 0; i < 1000; i++) {
 
     }
